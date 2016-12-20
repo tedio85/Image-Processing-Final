fe12d1d3adc07a522201ed51b22248490fa7b79f
Ted Yu <tedio85@yahoo.com.tw>
2016-12-20 11:03:06 +0800

    still working on histogram

--- findTurningPt.m
+++ findTurningPt.m
@@ -1,6 +1,13 @@
 function [FLm, Hm] = findTurningPt(input)
+    % The input should be the Y(luminance) of the image(grayscale)
 
-
-
+    % Gaussian filter
+    var = 4;
+    filterSize = 6*var+1;
+    smoothed = imgaussfilt(input, var,'FilterSize', [filterSize filterSize]);
+    
+    % generate histogram
+    
+    
 end
 
