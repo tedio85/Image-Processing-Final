5bee817dca24b086961d302cf560c64b30a26ca3
Ted Yu <tedio85@yahoo.com.tw>
2016-12-22 00:20:36 +0800

    findTurningPt completed, not tested yet

--- findTurningPt.m
+++ findTurningPt.m
@@ -1,4 +1,4 @@
-function [FLm, Hm] = findTurningPt(input)
+function [FLm, Hm] = findTurningPt(input, Bf)
     % The input should be the Y(luminance) of the image(grayscale)
 
     % Gaussian filter
@@ -7,7 +7,45 @@ function [FLm, Hm] = findTurningPt(input)
     smoothed = imgaussfilt(input, var,'FilterSize', [filterSize filterSize]);
     
     % generate histogram
-    [counts, ] = imhist(smoothed);
+    [p, ] = imhist(smoothed);
+    
+    % generate series a,b
+    TH = 0.0013;
+    a = zeros(1, 256);
+    b = zeros(1, 255);
+    
+    for i=1:256
+        if(p(i)<=TH)
+            a(i) = i;
+        end
+    end
+    
+    for i=2:256
+        b(i-1) = abs(a(i) - a(i-1));
+    end
+    
+    % obtain set T
+    count=1;
+    T = zeros(1, 10);
+    for i=1:255
+        for j=1:256
+            if(b(i)==a(j) && b(i)~=0)
+                T(count) = j;
+                count = count+1;
+            end
+            if(b(i) == a(i+1))
+                T(count) = i+1;
+                count = count+1;
+            end
+        end
+    end
+    whos T;
+    
+    % FLm, Lm, Hm
+    tmp = (1:1:256);
+    Lm = sum(p(T(1):T(2)).*tmp(T(1):T(2))) / sum(p(T(1):T(2)));
+    Hm = sum(p(T(3):T(4)).*tmp(T(3):T(4))) / sum(p(T(3):T(4)));
+    FLm = Lm + Lm * Bf;
     
 end
 
