6843b3f4885e2f5e10aba05874e6c5dd4f1baaab
dubyko <dubyko@gmail.com>
2016-12-24 14:36:26 +0800

    transform image to YIQ color space

--- computeBhist.m
+++ computeBhist.m
@@ -1,19 +1,20 @@
 function [ Bhist ] = computeBhist( input )
-    G = rgb2gray(input);
     YIQ = rgb2ntsc(input);
-    I = YIQ(:,:,1);
-    [M,N] = size(I);
-    [p, ] = imhist(I);
+    Y = YIQ(:,:,1);
+    [M,N] = size(Y);
+    [p, ] = imhist(Y);
     p = transpose(p);
     p = p / (M * N);
     T1 = 60;
-    T2 = max( max(G) ) - 61;
+    Y = im2uint8(Y);
+    T2 = max( max(Y) ) - 61;
     sumPriority = sum(p(T1 : T2));
-    Bhist = -2.0408 * sumPriority + 1.0204;
     if sumPriority < 0.01
         Bhist = 1;
-    if sumPriority > 0.5
+    elseif sumPriority > 0.5
         Bhist = 0; 
+    else
+        Bhist = -2.0408 * sumPriority + 1.0204;
     end
 end
 
