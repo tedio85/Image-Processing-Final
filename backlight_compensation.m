0ebae72aac5bcb2cd018ee66ac21d7d545031d1a
dubyko <dubyko@gmail.com>
2017-01-16 18:35:46 +0800

    add computeBimage2

--- backlight_compensation.m
+++ backlight_compensation.m
@@ -1,14 +1,21 @@
-img = imread('./testImg/testImg9.jpg');
+img = imread('./testImg/testImg5.jpg');
 
 imgYIQ = rgb2ntsc(img);
 imgY = imgYIQ(:,:,1);
 
+Bimage2 = computeBimage2(imgY);
 Bimage = computeBimage(imgY);
 Bhist  = computeBhist(imgY);
+Bf2 = computeBf(Bimage2, Bhist);
 Bf = computeBf(Bimage, Bhist);
 
+Bf
+Bf2
+
+[FLm2, Hm2] = findTurningPt(imgY, Bf2);
 [FLm, Hm] = findTurningPt(imgY, Bf);
-compensated = compensationCurve(round(FLm), round(Hm-60), imgY);
+compensated2 = compensationCurve(round(FLm2), round(Hm2), imgY);
+compensated = compensationCurve(round(FLm), round(Hm), imgY);
 
 imgYIQ(:,:,1) = compensated;
 imgYIQ(:,:,1) = double(imgYIQ(:,:,1)) / 255;
@@ -16,4 +23,10 @@ result = ntsc2rgb(imgYIQ);
 figure(1)
 imshow(img);
 figure(3)
+imshow(result);
+
+imgYIQ(:,:,1) = compensated2;
+imgYIQ(:,:,1) = double(imgYIQ(:,:,1)) / 255;
+result = ntsc2rgb(imgYIQ);
+figure(7)
 imshow(result);
