3da52e82818905d3780f8721e509168c8e9ef450
Ted <tim22.yu@hotmail.com>
2017-01-19 20:36:16 +0800

    modified histogram_equalization

--- histogram_equalization.m
+++ histogram_equalization.m
@@ -1,6 +1,10 @@
-img = imread('./testImg/testImg8.jpg');
+img = imread('./testImg/testImg.tif');
 imgYIQ = rgb2ntsc(img);
 imgYIQ(:,:,1) = histeq(imgYIQ(:,:,1));
 result = ntsc2rgb(imgYIQ);
 figure(1)
+title('histogram equalization');
 imshow(result);
+figure(2)
+imhist(imgYIQ(:,:,1));
+title('histogram');
